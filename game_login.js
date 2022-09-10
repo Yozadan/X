738bbb64d78200ba7bb3da82b7e8c9341bd9b562
Yozadan <98656290+Yozadan@users.noreply.github.com>
2022-09-10 14:04:52 -0400

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,8 @@
+function addUser()
+{
+    player1_name=document.getElementById("Player1_Name_input").value;
+    player2_name=document.getElementById("Player2_Name_input").value;
+localStorage.setItem("player1_name",player1_name);
+localStorage.setItem("player2_name",player2_name);
+window.location="game_page.html";
+}
